0cc7c5fbf5de61eec421bbaa3ff6ff47967a3c24
gaubert.julien <gaubert.julien@93cad5ea-ccb6-11de-8626-0d8cb8b546e2>
2009-02-17 22:13:16 +0000

    + Partial dev of CharacterSheetFactory and CharacterSheetParser + Class Constants to stock the constants related to the model package + Class TestCharacterSheetFactory

--- trunk/src/main/org/jevemon/model/charactersheet/CharacterSheetFactory.java
+++ trunk/src/main/org/jevemon/model/charactersheet/CharacterSheetFactory.java
@@ -1,9 +1,49 @@
 package org.jevemon.model.charactersheet;
 
+import org.jdom.Document;
+
+import be.fomp.jeve.core.api.LimitedAPI;
+import be.fomp.jeve.core.api.connectors.JEveConnectionFactory;
+import be.fomp.jeve.core.exceptions.*;
+
+
+
 public class CharacterSheetFactory {
 	
-	public CharacterSheet createCharacter(){
-		CharacterSheet result = new CharacterSheet();
+	private static final String FILE_PATH = "";
+
+	public static CharacterSheet createLimitedCharacterSheet( int userID, String apiKey, int characterID){
+		
+		LimitedAPI con = null;
+		
+		// create limited connection
+		try {
+			con = JEveConnectionFactory.createFactory().getLimitedConnection();
+		} catch (JEveConfigurationException e) {
+			// TODO handle this exception
+			e.printStackTrace();
+		}
+		
+		// get character sheet and cache it
+		try {
+			System.out.print("Fetching Character Sheet...");
+			Document doc = con.getCharacterSheet(userID, apiKey, characterID);
+			FileHandler.writeXmlFile(doc, FILE_PATH, "CharacterSheet.xml");
+			System.out.println("success");
+		} catch (JEveConnectionException e) {
+			// TODO handle this exception
+			e.printStackTrace();
+		} catch (JEveParseException e) {
+			// TODO handle this exception
+			e.printStackTrace();
+		} catch (JEveException e) {
+			// TODO handle this exception
+			e.printStackTrace();
+		}
+		
+		// TODO create character sheet from XML parse
+		CharacterSheetParser p = new CharacterSheetParser();
+		CharacterSheet result = p.parse();
 		return result;
 	}
 }
--- trunk/src/main/org/jevemon/model/charactersheet/CharacterSheetParser.java
+++ trunk/src/main/org/jevemon/model/charactersheet/CharacterSheetParser.java
@@ -2,4 +2,9 @@ package org.jevemon.model.charactersheet;
 
 public class CharacterSheetParser {
 
+	public CharacterSheet parse() {
+		// TODO Code this parser
+		return null;
+	}
+
 }
