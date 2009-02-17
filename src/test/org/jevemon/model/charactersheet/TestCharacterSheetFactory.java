f0c26e478ff62ac87a5dc760e7c39a4ef7e05e6e
gaubert.julien <gaubert.julien@93cad5ea-ccb6-11de-8626-0d8cb8b546e2>
2009-02-17 22:14:19 +0000

    - Forgot this in the last commit

---
+++ src/test/org/jevemon/model/charactersheet/TestCharacterSheetFactory.java
@@ -0,0 +1,20 @@
+package org.jevemon.model.charactersheet;
+
+public class TestCharacterSheetFactory {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		
+		//TODO make it works
+		CharacterSheet c = CharacterSheetFactory.createLimitedCharacterSheet(
+				3173522, 
+				"39D32F567BC4491BA1FCC0F3D8EED9B2F52BFEC3A3DD4F66B33DF0FADB9DFABB",
+				1);
+		// TODO do something with this character model
+		
+
+	}
+
+}
