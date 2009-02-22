9ab6fca9a73b2ad8373626e8024daf6ac8abc5ee
diabeteman <diabeteman@93cad5ea-ccb6-11de-8626-0d8cb8b546e2>
2009-02-22 18:08:34 +0000

    + SkillBrowser

---
+++ trunk/src/main/org/jevemon/view/skillbrowser/SkillTreeElement.java
@@ -0,0 +1,21 @@
+package org.jevemon.view.skillbrowser;
+
+import java.util.List;
+
+import com.trolltech.qt.gui.QTreeWidget;
+import com.trolltech.qt.gui.QTreeWidgetItem;
+
+public class SkillTreeElement extends QTreeWidgetItem {
+	
+	public SkillTreeElement(QTreeWidget parent, List<String> groups){
+		super(parent,groups);
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+}
